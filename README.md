7a178a69541c834ea00f0fce98be8bc7c3e61625
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-03 12:05:34 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Javascript Shape Console
---
+++ index.js
@@ -0,0 +1,14 @@
+const rectangleWidth = 200
+const rectangleLength = 100
+
+const rectangleArea = rectangleLength * rectangleWidth
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`);
+
+
+const length = prompt("How much length ?");
+const width = prompt("how much width ?");
+const height = prompt("how much height ?");
+
+const calc = length * width * height;
+ alert(`The result from ${length} * ${width} * ${height} = ${calc}`);
